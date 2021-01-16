0a683d49af44387e32bf435b1eaa535dbd463d2b
Sergio Baidon Carrillo <sergca@amazon.com>
2021-01-16 14:16:07 -0800

    Add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,9 +42,9 @@
 
 <h1>Great success!</h1>
 
-<figure>
+<figure class="bg-purple-500">
 	<img alt="Success Kid" src="{successkid}">
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live</strong></p>
+<p><strong>Try this file (src/routes/index.svelte)</strong></p>
